9d4cd5c6468bce6be56e9f9369fde55417cf9f39
Rajendra Balavant Jondhalekar <rajendra.10828096@ltimindtree.com>
2025-01-03 18:40:17 +0530

    jest Testing and Learning

--- Z-JEST-TESTING-FRAMEWORK/capsLock.test.js
+++ Z-JEST-TESTING-FRAMEWORK/capsLock.test.js
@@ -2,5 +2,5 @@ const capsLock = require("./capLock")
 
 
 test('Should capslock give name', () => {
-    expect(capsLock('neeraja')).toEqual({name:'NEERAJA'})
+    expect(capsLock('sham')).toEqual({name:'sham'})
 })
